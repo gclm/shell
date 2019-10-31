5d2fdcd14aa8e47e19d101aad415ed5d12ca3c9c
gclm <1719982754@qq.com>
2019-10-31 23:25:10 +0800

    增加 nexus 安装包

--- linux/linux.sh
+++ linux/linux.sh
@@ -20,23 +20,22 @@ Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 base_path="/usr/local"
 java_Path="$base_path/java"
 git_path="$base_path/git"
+nexus_path="$base_path/nexus"
 soft_path="/opt/software"
 module_path="/opt/module"
 
 # 版本
-shell_version="0.0.2"
+shell_version="0.0.3"
 maven_version="3.6.2"
 jdk_version="221"
 tomcat_version="9.0.19"
 gradle_version=""
-nuxus_version=""
+nuxus_version="3.19.1-01"
 mysql_version=""
 git_version="2.9.5"
 
 # 远程安装包地址
-github="https://raw.githubusercontent.com/gclm/shell/master"
 coding="https://dev.tencent.com/u/gclm/p/shell/git/raw/master"
-
 
 #==================基础配置 end =============================
 
@@ -44,6 +43,9 @@ coding="https://dev.tencent.com/u/gclm/p/shell/git/raw/master"
 #############系统开发环境组件 start #############
 
 java(){
+
+    init
+
     # 初始化安装目录
     if [ ! -d "$java_Path" ]; then
         echo -e "正在创建$java_Path目录"
@@ -61,8 +63,40 @@ compile(){
 
 #================== git =============================
 
+Nexus(){
+
+    init
+
+    # 初始化安装目录
+    if [ ! -d "$nexus_path" ]; then
+        echo -e "正在创建$nexus_path"
+        mkdir -p $nexus_path
+        echo -e "目录$nexus_path"
+    fi
+
+    # 增加新用户
+    groupadd nexus
+    useradd -g nexus nexus
+
+    cd $soft_path
+
+    nexus_file=$(ls | grep nexus-*-unix.tar.gz)
+    Nexus_dirname="nexus-${Nexus_version}"
+
+    if [ ! -f "$nexus_file" ]; then
+        echo -e "${Info}:正在下载 nexus 请稍等..."
+        wget -N --no-check-certificate ${coding}/linux/nexus/nexus-${Nexus_version}-unix.tar.gz
+    fi
+
+    tar zxvf nexus-${Nexus_version}-unix.tar.gz -C $nexus_path
+    
+}
+
 # 安装 git
 git_install(){
+
+    init
+
     echo -e "${Info}: 开始安装 Git v${git_version}"
 
     git_uninstall
@@ -94,6 +128,7 @@ git_install(){
 
     echo -e "${Info}:配置环境变量"
     echo "export PATH=$PATH:/usr/local/git/bin:" >> /etc/profile
+
     source /etc/profile
 
     echo -e "${Info}:测试是否安装成功"
@@ -141,10 +176,10 @@ git_uninstall(){
 # oracle  jdk install
 jdk(){
 
-    jdk_uninstall
-
     java
 
+    jdk_uninstall
+    
     cd $soft_path
     
     jdk_file=$(ls | grep jdk-*-linux-*.gz)
@@ -162,6 +197,7 @@ jdk(){
     echo -e "export JAVA_HOME=$java_Path/$jdk_dirname" >> /etc/profile
     echo -e "export CLASSPATH=.:\$JAVA_HOME/lib/dt.jar:\$JAVA_HOME/lib/tools.jar" >> /etc/profile
     echo -e "export PATH=\$PATH:\$JAVA_HOME/bin:" >> /etc/profile
+
     source /etc/profile
 
     jdk_test
@@ -192,18 +228,22 @@ jdk_uninstall(){
 	yum remove -y java-1.8.0-openjdk-headless java-1.8.0-openjdk java-1.8.0-openjdk-devel 
 	rm -rf $java_Path/jdk1.8.0_${jdk_version}
     rm -rf $soft_path/jdk-8u${jdk_version}-linux-x64.tar.gz
-    sed -i '/JAVA_HOME/d' /etc/profile
-    source /etc/profile
+    # 判断是否有需要删除的字符串
+    if sed -n '/JAVA_HOME/p' /etc/profile > /dev/null ; then
+        sed -i '/JAVA_HOME/d' /etc/profile
+        source /etc/profile
+    fi
 }
 
 #================== maven =============================
 
 # maven install
 maven(){
-    echo -e "${Tip}安装maven 之前必须安装 JDK"
-    
+
     java
 
+    echo -e "${Tip}安装maven 之前必须安装 JDK"
+    
     maven_uninstall
 
     cd $soft_path
