36f2e4b0ee69c7ca512c611ba7e1957116438df8
gclm <1719982754@qq.com>
2019-12-16 10:46:16 +0800

    优化 vultr 脚本

--- other/vultr/ping.sh
+++ other/vultr/ping.sh
@@ -6,53 +6,64 @@ Red="\033[31m"
 Font="\033[0m"
 Blue="\033[36m"
 
-echo -e "${Blue}Location: Frankfurt, DE${Font} "
-ping -c 5 fra-de-ping.vultr.com	
- 
-echo -e "${Blue}Location: Paris, France ${version}${Font} "
-ping -c 5 par-fr-ping.vultr.com	
- 
-echo -e "${Blue}Location: Amsterdam, NL${Font} "
-ping -c 5 ams-nl-ping.vultr.com	
- 
-echo -e "${Blue}Location: London, UK${Font} "
-ping -c 5 lon-gb-ping.vultr.com	
- 
-echo -e "${Blue}Location: Singapore${Font} "
-ping -c 5 sgp-ping.vultr.com	
- 
-echo -e "${Blue}Location: New York (NJ)${Font} "
-ping -c 5 nj-us-ping.vultr.com	
- 
-echo -e "${Blue}Location: Toronto, Canada${Font} "
-ping -c 5 tor-ca-ping.vultr.com	
- 
-echo -e "${Blue}Location: Tokyo, Japan${Font} "
-ping -c 5 hnd-jp-ping.vultr.com	
-  
-echo -e "${Blue}Location: Chicago, Illinois ${Font} "
-ping -c 5 il-us-ping.vultr.com	
- 
-echo -e "${Blue}Location: Atlanta, Georgia ${Font} " 
-ping -c 5 ga-us-ping.vultr.com	
-  
-echo -e "${Blue}Location: Miami, Florida${Font} "
-ping -c 5 fl-us-ping.vultr.com	
-  
-echo -e "${Blue}Location: Seattle, Washington${Font} "
-ping -c 5 wa-us-ping.vultr.com	
- 
-echo -e "${Blue}Location: Dallas, Texas ${Font} "
-ping -c 5 tx-us-ping.vultr.com	
-  
-echo -e "${Blue}Location: Silicon Valley, California ${Font} "
-ping -c 5 sjo-ca-us-ping.vultr.com	
-  
-echo -e "${Blue}Location: Los Angeles, California	${Font} "
-ping -c 5 lax-ca-us-ping.vultr.com	
-  
-echo -e "${Blue}Location: Sydney, Australia ${version}${Font} "
-ping -c 5 syd-au-ping.vultr.com	
- 
-echo -e "${Blue}Location: Sydney, Australia ${version}${Font} "
-ping -c 5 fra-de-ping.vultr.com
+echo "========================================="
+echo "From Local to Vultr 15 DCs Ping Test "
+echo "========================================="
+
+echo -e "${Blue}01 Tokyo${Font}"
+ping -c 10 hnd-jp-ping.vultr.com 
+echo "============================="
+echo -e "${Blue}02 Singapore${Font}"
+ping -c 10 sgp-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}03 Amsterdam${Font}"
+ping -c 10 ams-nl-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}04 Paris${Font}"
+ping -c 10 par-fr-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}05 Frankfurt${Font}"
+ping -c 10 fra-de-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}06 London${Font}"
+ping -c 10 lon-gb-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}07 New York${Font}"
+ping -c 10 nj-us-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}08 Chicago${Font}"
+ping -c 10 il-us-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}09 Dallas${Font}"
+ping -c 10 tx-us-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}10 Atlanta${Font}"
+ping -c 10 ga-us-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}11 Los Angeles${Font}"
+ping -c 10 lax-ca-us-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}12 Miami${Font}"
+ping -c 10 fl-us-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}13 Seattle${Font}"
+ping -c 10 wa-us-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}14 Silicon Valley${Font}"
+ping -c 10 sjo-ca-us-ping.vultr.com 
+
+echo "============================="
+echo -e "${Blue}15 Sydney${Font}"
+ping -c 10 syd-au-ping.vultr.com 
