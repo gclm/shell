06083c01cfde74ef5d5fc0a206e0fdb5729187fb
gclm <1719982754@qq.com>
2020-01-25 16:24:28 +0800

    feat(优化 shell 脚本升级操作):

--- linux/install.sh
+++ linux/install.sh
@@ -39,16 +39,10 @@ coding="https://gclm.coding.net/p/LinuxSoftwares/d/LinuxSoftwares/git/raw/master
 shell="https://gclm.coding.net/p/shell/d/shell/git/raw/master/linux"
 #==================基础配置 end =============================
 
-# 获取执行脚本
+# 执行相关脚本
+init(){
 wget ${shell}/init.sh  && chmod +x init.sh
 wget ${shell}/axel.sh  && chmod +x axel.sh
-wget ${shell}/shell.sh  && chmod +x shell.sh
-
-# 执行相关脚本
-
-. ./shell.sh
-
-init(){
 . ./init.sh
 . ./axel.sh
 }
@@ -92,6 +86,27 @@ jenkins(){
     yum install -y jenkins
 }
 
+#更新脚本
+update_shell(){
+	echo -e "当前版本为 [ ${shell_version} ]，开始检测最新版本..."
+	shell_new_version=$(wget --no-check-certificate -qO- "http://${coding}/linux/install.sh"|grep 'shell_version="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
+	[[ -z ${shell_new_version} ]] && echo -e "${Error} 检测最新版本失败 !" && start_menu
+	if [[ ${shell_new_version} != ${shell_version} ]]; then
+		echo -e "发现新版本[ ${shell_new_version} ]，是否更新？[Y/n]"
+		read -p "(默认: y):" yn
+		[[ -z "${yn}" ]] && yn="y"
+		if [[ ${yn} == [Yy] ]]; then
+			wget -N --no-check-certificate http://${github}/linux/install.sh && chmod +x install.sh
+			echo -e "脚本已更新为最新版本[ ${shell_new_version} ] !"
+		else
+			echo && echo "	已取消..." && echo
+		fi
+	else
+		echo -e "当前已是最新版本[ ${shell_new_version} ] !"
+		sleep 5s
+	fi
+}
+
 #开始菜单
 start_menu(){
 clear
@@ -128,7 +143,7 @@ case "$num" in
 	update_shell
 	;;
 	2)
-	#	rm -rf *.sh
+	rm -rf *.sh
 	exit 1
 	;;
 	11)
