a64a9c62d3afb106ad5bdb734f47f093007d54b1
gclm <1719982754@qq.com>
2020-01-27 14:30:33 +0800

    feat(重命名  install 为 Linux):

--- linux/Linux.sh
+++ linux/Linux.sh
@@ -89,14 +89,14 @@ jenkins(){
 #更新脚本
 update_shell(){
 	echo -e "当前版本为 [ ${shell_version} ]，开始检测最新版本..."
-	shell_new_version=$(wget --no-check-certificate -qO- "http://${coding}/linux/install.sh"|grep 'shell_version="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
+	shell_new_version=$(wget --no-check-certificate -qO- "http://${coding}/linux/Linux.sh"|grep 'shell_version="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
 	[[ -z ${shell_new_version} ]] && echo -e "${Error} 检测最新版本失败 !" && start_menu
 	if [[ ${shell_new_version} != ${shell_version} ]]; then
 		echo -e "发现新版本[ ${shell_new_version} ]，是否更新？[Y/n]"
 		read -p "(默认: y):" yn
 		[[ -z "${yn}" ]] && yn="y"
 		if [[ ${yn} == [Yy] ]]; then
-			wget -N --no-check-certificate http://${github}/linux/install.sh && chmod +x install.sh
+			wget -N --no-check-certificate http://${github}/linux/Linux.sh && chmod +x Linux.sh
 			echo -e "脚本已更新为最新版本[ ${shell_new_version} ] !"
 		else
 			echo && echo "	已取消..." && echo
